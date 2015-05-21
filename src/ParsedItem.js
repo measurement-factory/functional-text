ffe4e8f24ac780a66a2f2029b3db89495b93341c
Mark-Simulacrum <mark.simulacrum@gmail.com>
2015-05-21 10:36:27 -0600

    Remove files that are no longer used.

--- src/ParsedItem.js
+++
@@ -1,9 +0,0 @@
-export default class ParsedItem {
-    constructor({children}) {
-        this.type = "parsed-item";
-        this.children = children;
-    }
-    toString() {
-        return global.JSON_OUTPUT ? JSON.stringify(this, undefined, 4) : this.children.join("");
-    }
-}
